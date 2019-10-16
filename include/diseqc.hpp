24af414f661b9dbbbdc66006467fd5ecd87cf112
Dr. Jochen Manns <manns@insynergie.de>
2019-10-16 12:29:26 +0200

    Review & docu diseqc

--- include/diseqc.hpp
+++ include/diseqc.hpp
@@ -3,31 +3,46 @@
 
 #include <linux/types.h>
 
+// Unterstützte DiSQeC Befehle.
 enum diseqc_modes
 {
+    // Keine Schaltung.
+    none = 0,
+    // Burst-Mode.
     burst_off = 5,
     burst_on = 6,
+    // Standard.
     diseqc1 = 1,
     diseqc2 = 2,
     diseqc3 = 3,
     diseqc4 = 4,
-    none = 0,
 };
 
+/*
+    Beschreibt eine DiSEqC Steuermeldung.
+*/
 class DiSEqCMessage
 {
 public:
-    DiSEqCMessage(__u8 message[], __u8 burst, __u8 repeat) : message(message), burst(burst), repeat(repeat) {}
+    // Erzeugt eine Burst-Mode Steuermeldung.
+    DiSEqCMessage(bool burst) : message(nullptr), burst(burst), repeat(1) {}
+    // Erzeugt eine Standardsteuermeldung.
+    DiSEqCMessage(__u8 message[], __u8 repeat) : message(message), burst(false), repeat(repeat) {}
 
 public:
+    // Nachricht - falls nicht der Burst-Mode verwendet wird.
     const __u8 *message;
-    const __u8 burst;
+    // Burst-Mode - nur berücksichtigt wenn die Nachricht nicht gesetzt ist.
+    const bool burst;
+    // Die Anzahl der Wiederholungen - zurzeit nicht verwendet.
     const __u8 repeat;
 
 public:
+    // Überträgt die Meldung an ein Frontend.
     int send(int fd);
 
 public:
+    // Erstellt eine neue Steuermeldung.
     static DiSEqCMessage create(diseqc_modes mode, bool highFrequency, bool horizontal);
 };
 
--- src/diseqc.cpp
+++ src/diseqc.cpp
@@ -4,13 +4,18 @@
 #include <string.h>
 #include <sys/ioctl.h>
 
+/*
+    Übermittelt einen DiSEqC Steuerbefehl an ein Frontend.
+*/
 int DiSEqCMessage::send(int fd)
 {
+    // Burst-Mode Umschaltung.
     if (message == nullptr)
     {
         return ::ioctl(fd, FE_DISEQC_SEND_BURST, burst ? fe_sec_mini_cmd::SEC_MINI_B : fe_sec_mini_cmd::SEC_MINI_A);
     }
 
+    // Standardumschaltung.
     dvb_diseqc_master_cmd cmd = {{0}, static_cast<__u8>((repeat == 1) ? 3 : 4)};
 
     ::memcpy(cmd.msg, message, cmd.msg_len);
@@ -18,8 +23,12 @@ int DiSEqCMessage::send(int fd)
     return ::ioctl(fd, FE_DISEQC_SEND_MASTER_CMD, &cmd);
 }
 
+/*
+    Erstellt die Beschreibung für einen DiSEqC Befehl.
+*/
 DiSEqCMessage DiSEqCMessage::create(diseqc_modes mode, bool highFrequency, bool horizontal)
 {
+    // Vorauswahl für die Standardumschaltung.
     __u8 choice = 0;
 
     if (highFrequency)
@@ -27,22 +36,23 @@ DiSEqCMessage DiSEqCMessage::create(diseqc_modes mode, bool highFrequency, bool 
     if (horizontal)
         choice |= 0x02;
 
+    // Nachrichten je nach Art der DiSEqC Steuerung (der Einfachheit halber aus DVB.NET übernommen).
     switch (mode)
     {
     case diseqc_modes::none:
-        return DiSEqCMessage(new __u8[3]{0xe0, 0x00, 0x00}, 0xff, 1);
+        return DiSEqCMessage(new __u8[3]{0xe0, 0x00, 0x00}, 1);
     case diseqc_modes::burst_on:
-        return DiSEqCMessage(nullptr, 0, 1);
+        return DiSEqCMessage(true);
     case diseqc_modes::burst_off:
-        return DiSEqCMessage(nullptr, 1, 1);
+        return DiSEqCMessage(false);
     case diseqc_modes::diseqc1:
-        return DiSEqCMessage(new __u8[4]{0xe0, 0x10, 0x38, static_cast<__u8>(0xf0 | choice)}, 0xff, 3);
+        return DiSEqCMessage(new __u8[4]{0xe0, 0x10, 0x38, static_cast<__u8>(0xf0 | choice)}, 3);
     case diseqc_modes::diseqc2:
-        return DiSEqCMessage(new __u8[4]{0xe0, 0x10, 0x38, static_cast<__u8>(0xf4 | choice)}, 0xff, 3);
+        return DiSEqCMessage(new __u8[4]{0xe0, 0x10, 0x38, static_cast<__u8>(0xf4 | choice)}, 3);
     case diseqc_modes::diseqc3:
-        return DiSEqCMessage(new __u8[4]{0xe0, 0x10, 0x38, static_cast<__u8>(0xf8 | choice)}, 0xff, 3);
+        return DiSEqCMessage(new __u8[4]{0xe0, 0x10, 0x38, static_cast<__u8>(0xf8 | choice)}, 3);
     case diseqc_modes::diseqc4:
-        return DiSEqCMessage(new __u8[4]{0xe0, 0x10, 0x38, static_cast<__u8>(0xfc | choice)}, 0xff, 3);
+        return DiSEqCMessage(new __u8[4]{0xe0, 0x10, 0x38, static_cast<__u8>(0xfc | choice)}, 3);
     default:
         throw "unsupported DiSEqC mode";
     }
