a29f0dcded75275a61ff9233f06ff481255ba221
Dr. Jochen Manns <manns@insynergie.de>
2019-10-18 00:37:00 +0200

    remove test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 //#define DUMP_STRUCT_LAYOUT
-#define RUN_TEST
 
 #include <fcntl.h>
 #include <netdb.h>
@@ -61,62 +60,6 @@ int main()
     }
 
     ::printf("listener started\n");
-
-#ifdef RUN_TEST
-    auto server = ::gethostbyname("localhost");
-
-    sockaddr_in addr = {.sin_family = AF_INET, .sin_port = ::htons(29713), {0}};
-
-    ::memcpy(&addr.sin_addr.s_addr, server->h_addr, server->h_length);
-
-    auto fd = socket(AF_INET, SOCK_STREAM, 0);
-    auto err = ::connect(fd, reinterpret_cast<sockaddr *>(&addr), sizeof(addr));
-
-    if (err != 0)
-    {
-        printf("connect: %d %d\n", err, errno);
-    }
-
-    auto cr = frontend_request::connect_adapter;
-    connect_request cr_data = {.adapter = 0, .frontend = 0};
-
-    ::write(fd, &cr, sizeof(cr));
-    ::write(fd, &cr_data, sizeof(cr_data));
-
-    SatelliteTune rtlplus = {
-        .lnbMode = diseqc_modes::diseqc1,
-        .lnb1 = 9750000,
-        .lnb2 = 10600000,
-        .lnbSwitch = 11700000,
-        .lnbPower = true,
-        .modulation = fe_modulation::QPSK,
-        .frequency = 12187500,
-        .symbolrate = 27500000,
-        .horizontal = true,
-        .innerFEC = fe_code_rate::FEC_3_4,
-        .s2 = false,
-        .rolloff = fe_rolloff::ROLLOFF_AUTO,
-    };
-
-    auto tr = frontend_request::tune;
-
-    ::write(fd, &tr, sizeof(tr));
-    ::write(fd, &rtlplus, sizeof(rtlplus));
-
-    ::sleep(1);
-
-    auto addsect = frontend_request::add_section_filter;
-    __u16 epg = 1800;
-
-    ::write(fd, &addsect, sizeof(addsect));
-    ::write(fd, &epg, sizeof(epg));
-
-    ::sleep(5);
-
-    printf("done\n");
-
-    ::close(fd);
-#endif
 
     manager.run();
 }
